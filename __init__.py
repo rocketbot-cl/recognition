40a27716a41f7fd7f211354a31716852957f9dc2
Caleb <calebcipra@gmail.com>
2021-08-06 15:14:47 -0400

    [hidden] File support added.

--- __init__.py
+++ __init__.py
@@ -115,6 +115,8 @@ if (platform_name == "Windows"):
         
 module = GetParams("module")
 
+filesSupported = ("jpeg","png","jpg", "pdf", "JPEG", "PNG", "JPG", "PDF")
+
 try:
 
     if (module == "folder_recognition"):
@@ -134,59 +136,56 @@ try:
         imageToSearchIn = None
         newPathImage = None
         converted = False
-        
-        if not (imageExtensionChecker(imageName)):
-            newPathImage = convertPdfToPng(imageToSearch, path_to_poppler)
-            imageToSearchIn = cv2.imread(newPathImage)
-            converted = True
+        if not imageName.endswith(filesSupported):
+            varWhereToSaveIn = GetParams("varWhereToSaveIn")
+            SetVar(varWhereToSaveIn, "File's type is not supported")
         else:
-            imageToSearchIn = cv2.imread(imageToSearch)
+            if not (imageExtensionChecker(imageName)):
+                newPathImage = convertPdfToPng(imageToSearch, path_to_poppler)
+                imageToSearchIn = cv2.imread(newPathImage)
+                converted = True
+            else:
+                imageToSearchIn = cv2.imread(imageToSearch)
 
 
-        for eachFolderToCompare in glob(folderToCompare, recursive=True):
-            result = eachFolderToCompare.replace("/", os.sep) + os.sep +"result.ini"
-            eachFolderToCompare = eachFolderToCompare.replace("/", os.sep) + os.sep +"**" + os.sep + "*"
-            for fileToCompareWith in glob(eachFolderToCompare, recursive=True):
-                if(fileToCompareWith != result):
-                    imageToCompareWith = cv2.imread(fileToCompareWith)
-
-                    # Create our ORB detector and detect keypoints and descriptors
-                    sift = cv2.SIFT_create()
-
-                    # Find the key points and descriptors with ORB
-                    keypoints1, descriptors1 = sift.detectAndCompute(imageToSearchIn, None)
-                    keypoints2, descriptors2 = sift.detectAndCompute(imageToCompareWith, None)
-
-                    bf = cv2.BFMatcher()
-                    matches = bf.knnMatch (descriptors2, descriptors1,k=2)
-
-                    good_matches = []
-
-                    for m1, m2 in matches:
-                        if (m1.distance < 0.6*m2.distance):
-                            good_matches.append([m1])
-                    
-                    # SIFT_matches = cv2.drawMatchesKnn(imageToCompareWith, keypoints2, imageToSearchIn, keypoints1, good_matches, None, flags=2)
-                    # cv2.imwrite("/home/keileb/myresult.png", SIFT_matches)
-
-                    if ((len(good_matches) > 20)):
-                        varWhereToSaveIn = GetParams("varWhereToSaveIn")
-                        file = open(result, "r")
-                        fileRead = file.read()
-                        SetVar(varWhereToSaveIn, fileRead)
-                        file.close()
+            for eachFolderToCompare in glob(folderToCompare, recursive=True):
+                result = eachFolderToCompare.replace("/", os.sep) + os.sep +"result.ini"
+                eachFolderToCompare = eachFolderToCompare.replace("/", os.sep) + os.sep +"**" + os.sep + "*"
+                for fileToCompareWith in glob(eachFolderToCompare, recursive=True):
+                
+                    if(fileToCompareWith != result):
+                        imageToCompareWith = cv2.imread(fileToCompareWith)
+                        # Create our ORB detector and detect keypoints and descriptors
+                        sift = cv2.SIFT_create()
+                        # Find the key points and descriptors with ORB
+                        keypoints1, descriptors1 = sift.detectAndCompute(imageToSearchIn, None)
+                        keypoints2, descriptors2 = sift.detectAndCompute(imageToCompareWith, None)
+                        bf = cv2.BFMatcher()
+                        matches = bf.knnMatch (descriptors2, descriptors1,k=2)
+                        good_matches = []
+                        for m1, m2 in matches:
+                            if (m1.distance < 0.6*m2.distance):
+                                good_matches.append([m1])
                         # SIFT_matches = cv2.drawMatchesKnn(imageToCompareWith, keypoints2, imageToSearchIn, keypoints1, good_matches, None, flags=2)
-                        # cv2.imwrite("/path/to/save/myresult.png", SIFT_matches)
-                        if (newPathImage != None):
-                            os.remove(newPathImage)
-                            newPathImage = None
-                            positiveMatch = 1
-                        break
-            if (positiveMatch == 1):
-                break
+                        # cv2.imwrite("/home/keileb/myresult.png", SIFT_matches)
+                        if ((len(good_matches) > 20)):
+                            varWhereToSaveIn = GetParams("varWhereToSaveIn")
+                            file = open(result, "r")
+                            fileRead = file.read()
+                            SetVar(varWhereToSaveIn, fileRead)
+                            file.close()
+                            # SIFT_matches = cv2.drawMatchesKnn(imageToCompareWith, keypoints2, imageToSearchIn, keypoints1, good_matches, None, flags=2)
+                            # cv2.imwrite("/path/to/save/myresult.png", SIFT_matches)
+                            if (newPathImage != None):
+                                os.remove(newPathImage)
+                                newPathImage = None
+                                positiveMatch = 1
+                            break
+                if (positiveMatch == 1):
+                    break
 
-        if (newPathImage != None):
-            os.remove(newPathImage)
+            if (newPathImage != None):
+                os.remove(newPathImage)
 
     if (module == "exact_match"):
 
